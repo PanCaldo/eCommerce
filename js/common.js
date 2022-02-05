afa41349d839b0294f92a924ee6d523fdebed306
Antonio <caldoukr@gmail.com>
2022-02-05 21:03:55 +0200

    add indexfour

--- js/common.js
+++ js/common.js
@@ -1,5 +1,37 @@
+let tabsContent = document.querySelectorAll('.tab__content');
+let tabsControl = document.querySelectorAll('.tab__item');
+
+tabsControl.forEach(elem => {
+    elem.addEventListener('click', function () {
+        showTabContent(elem.dataset.tabnumber);
+
+        tabsControl.forEach(elem => {
+            elem.classList.remove('tab__item-active')
+        });
+
+        this.classList.add('tab__item-active')
+    });
+});
+
+function hideTabContent() {
+    for (let i = 1; i < tabsContent.length; i++) {
+        tabsContent[i].classList.add('tab-hidden');
+    }
+};
+
+hideTabContent();
+
+function showTabContent(tabnumber) {
+    for (let i = 0; i < tabsContent.length; i++) {
+        tabsContent[i].classList.add('tab-hidden');
+    }
+    tabsContent[tabnumber - 1].classList.remove("tab-hidden");
+
+};
+
+
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+
     direction: 'horizontal',
     loop: true,
   
@@ -15,4 +47,66 @@ const swiper = new Swiper('.swiper', {
     freeMode: true,
     
 });
+
+
   
+var $range = $(".js-range-slider"),
+    $inputFrom = $(".js-input-from"),
+    $inputTo = $(".js-input-to"),
+    instance,
+    min = 0,
+    max = 1000,
+    from = 0,
+    to = 0;
+
+$range.ionRangeSlider({
+	skin: "round",
+  type: "double",
+  backgroundcolor: 'green',
+    min: min,
+    max: max,
+    from: 80,
+    to: 900,
+    onStart: updateInputs,
+    onChange: updateInputs
+});
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+	from = data.from;
+    to = data.to;
+    
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < min) {
+        val = min;
+    } else if (val > to) {
+        val = to;
+    }
+    
+    instance.update({
+        from: val
+    });
+});
+
+$inputTo.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < from) {
+        val = from;
+    } else if (val > max) {
+        val = max;
+    }
+    
+    instance.update({
+        to: val
+    });
+});
+
